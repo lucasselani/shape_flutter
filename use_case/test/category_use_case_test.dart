da5bf5aec5c6860c26f3d7aaa086d0bde5977b98
Lucas Selani de Melo <selanimelo95@gmail.com>
2020-05-28 21:02:11 -0300

    test: use_case mobx folder unit tests

---
+++ use_case/test/category_use_case_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:repository/repository.dart';
+import 'package:use_case/src/use_cases/category_use_case.dart';
+
+class MockRepository extends Mock implements JsonRepository {}
+
+void main() {
+  test('category use case list', () async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    var repository = MockRepository();
+    var useCase = CategoryUseCase(repository);
+    when(repository.getData()).thenAnswer((_) async => [Category()]);
+    var list = await useCase.getData();
+    expect(list.isNotEmpty, true);
+    expect(list is List<Category>, true);
+    expect(list[0] is Category, true);
+  });
+}
---
+++ use_case/test/taco_use_case_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:repository/repository.dart';
+import 'package:use_case/src/use_cases/taco_use_case.dart';
+
+class MockRepository extends Mock implements JsonRepository {}
+
+void main() {
+  test('category use case list', () async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    var repository = MockRepository();
+    var useCase = TacoUseCase(repository);
+    when(repository.getData()).thenAnswer((_) async => [Food()]);
+    var list = await useCase.getData();
+    expect(list.isNotEmpty, true);
+    expect(list is List<Food>, true);
+    expect(list[0] is Food, true);
+  });
+}
